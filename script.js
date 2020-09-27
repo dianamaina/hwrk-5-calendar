9a24bdcc9f00d25e560531da266d505f90452d07
diana maina <dianamainachui@gmail.com>
2020-09-27 07:51:36 -0400

    modified files

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-var buildingBlock = document.querySelector("#currentDay");
+function hourUpdater() {  var currentHour = moment().hours();
+
+// loop over time blocks
+$(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+    // check if we've moved past this time
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } 
+    else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } 
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+  }
+var buildingBlock = document.querySelector("#presentDay");
+var buildingBlock = document.querySelector("#Past");
+var buildingBlock = document.querySelector("#present");
+var buildingBlock = document.querySelector("#future");
 
 buildingBlock.innerHTML = moment();
 
@@ -12,6 +36,23 @@ for(i=0;i<saveButtons.length;i++){
 
     })
 }
+hourUpdater();
+
+$(document).ready(function() {listen for save button clicks $(".saveBtn").on("click", function() {get nearby values var value = $(this).siblings(".description").val(); var time = $(this).parent().attr("id");
+
+// save in localStorage
+var lcoalStarageData = localStorage.getItem("data")
+localStorage.setItem(time, value);
+});
+$("#hour-9 .description").val(localStorage.getItem("hour-9")); 
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13")); 
+$("#hour-14 .description").val(localStorage.getItem("hour-14")); 
+$("#hour-15 .description").val(localStorage.getItem("hour-15")); 
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));   
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 /*
 instructions:
